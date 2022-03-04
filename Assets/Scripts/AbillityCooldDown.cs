5be7053d9dc99e959ffe75652f379acfc0c49a2c
benjamus <carl.benjamin.dreyer@gmail.com>
2022-03-04 21:52:54 +0100

    Added Abillity cooldown overlay animation

--- Assets/Scripts/AbillityCooldDown.cs
+++ Assets/Scripts/AbillityCooldDown.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class AbillityCooldDown : MonoBehaviour
 {
     public GameObject overlayPref;
+    private Camera cam;
 
     private void Awake() {
         if (overlayPref == null) {
             Destroy(this);
         }
+        cam = Camera.main;
     }
 
     private void Start() {
@@ -22,10 +24,23 @@ public class AbillityCooldDown : MonoBehaviour
             if (GameManagerScript.Abillities[i].curCooldown < GameManagerScript.Abillities[i].Cooldown) { // While cooling down abillity    
                 GameManagerScript.Abillities[i].curCooldown += Time.deltaTime;
                 // Get the percentage of how much the overlay should cover of the buttton
-                float distFromCompl = 1 - GameManagerScript.Abillities[i].Cooldown / GameManagerScript.Abillities[i].curCooldown;
+                float distFromCompl = 1 - GameManagerScript.Abillities[i].curCooldown / GameManagerScript.Abillities[i].Cooldown;
+                UpdateOverlay(GameManagerScript.Abillities[i].Overlay, distFromCompl, GameManagerScript.Abillities[i].FraqScale, GameManagerScript.Abillities[i].Dist2Center, GameManagerScript.Abillities[i].StartPos.y);
+                //Debug.Log(distFromCompl);
 
             }
+            else { // Done with coolDown
+                GameManagerScript.Abillities[i].curCooldown = 0f;
+            }
         }
+    }
+
+    private void UpdateOverlay(GameObject overlay, float inter, float fraq, float dist2cent, float oriPos) { 
+        //Debug.Log($"Dist to center: {dist2cent}");
+        // Perform y-scale interpolation
+        overlay.transform.localScale = new Vector3(overlay.transform.localScale.x, fraq * inter * 100f, overlay.transform.localScale.z);
+        // Move sprite downwards to compensate from bottom contraction
+        overlay.transform.position = new Vector3(overlay.transform.position.x, oriPos - dist2cent * (1 - inter), overlay.transform.position.z);
     }
 
     /// <summary>
@@ -38,14 +53,24 @@ public class AbillityCooldDown : MonoBehaviour
 
         // Populate a new struct with info about the abillity (overlay prefab and ab cooldown)
         for (int i = 0; i < aInfo.Length; ++i) {
+            Debug.Log($"Rect position: {aInfo[i].GetComponent<RectTransform>().position}");
             Vector2 abPos = aInfo[i].transform.position;
             GameManagerScript.Abillities.Add(new Assets.AbillityInfo() {
                 Overlay = Instantiate(overlayPref, abPos, Quaternion.identity),
                 Cooldown = aInfo[i].GetComponent<AbInfo>().abillityCooldown,
-                curCooldown = 0f
+                curCooldown = 0f,
+                FraqScale = overlayPref.transform.localScale.y / 100f, // 1% of the overlay's scale, to scale it
+                Dist2Center = CalcDist2Center(aInfo[i]),
+                StartPos = aInfo[i].transform.position,
             });
             //Debug.Log($"Found a new abillity button with cooldown of: {GameManagerScript.Abillities[i].Cooldown}");
         }
+    }
+
+    private float CalcDist2Center(GameObject obj) {
+        float tmp = Mathf.Abs(obj.transform.position.y - obj.transform.GetChild(1).transform.position.y);
+        Debug.Log($"Calculated dist to center: {tmp}");
+        return tmp;
     }
 
     private GameObject[] GetChildren(GameObject obj) {
@@ -55,5 +80,12 @@ public class AbillityCooldDown : MonoBehaviour
             temp[i] = obj.transform.GetChild(i).gameObject;
         }
         return temp;
+    }
+
+    private float ConvertPx2Units(float p) {
+        float ortho = cam.orthographicSize;
+        float pixelH = cam.pixelHeight;
+
+        return (p * ortho * 2f) / pixelH;
     }
 }

eaa7cf9b274aead0723fffc14e64e64e9992542e
CharleezGithub <charleegithub@gmail.com>
2022-03-04 18:42:46 +0100

    sus removed

--- Assets/Scripts/sword_attack.cs
+++ Assets/Scripts/sword_attack.cs
@@ -6,19 +6,8 @@ public class sword_attack : MonoBehaviour
 {
     public GameObject Sword;
 
-    void start()
-    {
-        Sword.transform.rotation = new Quaternion(0, 0, 55, 0);
-    }
-    IEnumerator Sus()
-    { 
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            Sword.transform.rotation = new Quaternion(0, 0, -10, 0);
-        }
-    }
     private void Update()
     {
-
+        
     }
 }

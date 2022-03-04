01f687f116cd6a99e37baa27845af8f260869c50
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-03-04 20:27:07 +0100

    added displaying of tower levels

--- Assets/Scripts/TowerEventHandler.cs
+++ Assets/Scripts/TowerEventHandler.cs
@@ -1,3 +1,4 @@
+using Assets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,24 @@ public class TowerEventHandler : MonoBehaviour
 {
     private void Start()
     {
+        int towerLevel = GameManagerScript.Bases[GetComponent<TowerIndexHolder>().TowerIndex].TowerLevel;
+        BuildingType type = GameManagerScript.Bases[GetComponent<TowerIndexHolder>().TowerIndex].Type;
 
+        var sp = GetComponent<SpriteRenderer>();
+        switch (type)
+        {
+            case BuildingType.Player:
+                sp.sprite = GameManagerScript.PlayerTowerSprites[towerLevel - 1];
+                break;
+
+            case BuildingType.Enemy:
+                sp.sprite = GameManagerScript.EnemyTowerSprites[0];
+                break;
+
+            default:
+                sp.sprite = GameManagerScript.NeutralTowerSprites[towerLevel - 1];
+                break;
+        }
     }
 
     public void OnMouseEnter()

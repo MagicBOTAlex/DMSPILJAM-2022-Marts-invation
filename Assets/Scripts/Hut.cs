c11abffe0eb75b3d1e111a08a97a77eb682d1c58
benjamus <carl.benjamin.dreyer@gmail.com>
2022-03-04 16:09:07 +0100

    Menu scene

--- Assets/Scripts/Hut.cs
+++ Assets/Scripts/Hut.cs
@@ -6,6 +6,7 @@ public class Hut : MonoBehaviour
 {
     // Spawn 1 unit pr. sec
     private float spawnRate = 1f;
+    private uint units = 0;
     [SerializeField]
     private GameObject unit;
 
@@ -23,16 +24,19 @@ public class Hut : MonoBehaviour
         HandleUnits();
     }
 
-
+     /// <summary>
+    /// Handles spawning units each physics update 
+    /// </summary>
     private void HandleUnits() {
         spawnCooldown += Time.fixedDeltaTime;
         if (spawnCooldown >= spawnRate) {
             // Should spawn a unit
+            SpawnUnit();
         }
     }
 
     /// <summary>
-    /// 
+    /// Method for spawning a unit, inside the hub
     /// </summary>
     private void SpawnUnit() {
         

122025afb34cfd2989cf70c03fa63cf5a3b6d40a
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-03-04 21:43:10 +0100

    Attacking with units (Part 1)

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -17,14 +17,23 @@ public class GameManagerScript : MonoBehaviour
     [Header("Settings")]
     public float GainUnitsDelay_ = 1f;
     public static float GainUnitsDelay { get { return instance.GainUnitsDelay_; } set { instance.GainUnitsDelay_ = value; } }
+    public float UnitSpeed_ = 1f;
+    public static float UnitSpeed { get { return instance.UnitSpeed_; } set { instance.UnitSpeed_ = value;} }
+    public float UnitSpawnDelay_ = 0.1f;
+    public static float UnitSpawnDelay { get { return instance.UnitSpawnDelay_; } set { instance.UnitSpawnDelay_ = value; } }
 
-    [Header("Sprites")]
+    [Header("Sprites and prefabs")]
     public Sprite[] PlayerTowerSprites_;
     public static Sprite[] PlayerTowerSprites { get { return instance.PlayerTowerSprites_; } set { instance.PlayerTowerSprites_ = value; } }
     public Sprite[] NeutralTowerSprites_;
     public static Sprite[] NeutralTowerSprites { get { return instance.NeutralTowerSprites_; }  set { instance.NeutralTowerSprites_ = value; } }
     public Sprite[] EnemyTowerSprites_;
     public static Sprite[] EnemyTowerSprites { get { return instance.EnemyTowerSprites_; } set { instance.EnemyTowerSprites_ = value; } }
+
+    public GameObject UnitPrefab;
+
+    public Sprite PlayerUnitSprite;
+    public Sprite EnemyUnitSprite;
 
     private void Start()
     {
@@ -56,6 +65,41 @@ public class GameManagerScript : MonoBehaviour
                     Towers[i].UnitsInside++;
                 }
             }
+        }
+    }
+
+    /// <summary>
+    /// Sends a certain amount of units from a tower to another
+    /// </summary>
+    /// <param name="from">Where the units shall be drawn from</param>
+    /// <param name="to">Where the units shall endup at</param>
+    /// <param name="amount">How many units shall be sent</param>
+    /// <returns>True if sufficiant amount in "from" tower</returns>
+    public bool SendUnits(TowerInfo from, TowerInfo to, int amount)
+    {
+        if (from.UnitsInside < amount) return false;
+        if (from.Type == TowerType.Neutral) return false;
+
+        StartCoroutine(StartSending(from, to, amount));
+        return true;
+    }
+
+    IEnumerator StartSending(TowerInfo from, TowerInfo to, int amount)
+    {
+        var unitSprite = (from.Type == TowerType.Player) ? PlayerUnitSprite : EnemyUnitSprite;
+
+        for (int i = 0; i < amount; i++)
+        {
+            var unit = Instantiate(UnitPrefab, from.Object.transform.position, Quaternion.identity) as GameObject;
+            var unitScript = unit.GetComponent<UnitScript>();
+
+            unit.GetComponent<SpriteRenderer>().sprite = unitSprite;
+
+            unitScript.From = from.Object.transform.position;
+            unitScript.To = to.Object.transform.position;
+            unitScript.Speed = UnitSpeed_;
+
+            yield return new WaitForSecondsRealtime(UnitSpawnDelay_);
         }
     }
 }

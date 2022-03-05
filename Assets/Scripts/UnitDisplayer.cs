01b3dab5650da7841a05a8efc09afe6c2cc69bd4
benjamus <carl.benjamin.dreyer@gmail.com>
2022-03-05 13:27:05 +0100

    s

--- Assets/Scripts/UnitDisplayer.cs
+++ Assets/Scripts/UnitDisplayer.cs
@@ -7,16 +7,19 @@ public class UnitDisplayer : MonoBehaviour
     public int towerIndex = -1;
     private RectTransform canvasTrans;
     private Text text;
-
+    Camera cam;
     private void Awake() {
         canvasTrans = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        cam = Camera.main;
     }
     private void Start() {
         // Create text field, so we use the main canvas 
 
         RectTransform trans = gameObject.AddComponent<RectTransform>();
         transform.SetParent(canvasTrans);
-        trans.anchoredPosition = Camera.main.WorldToScreenPoint(transform.position);
+        Debug.Log(transform.position);
+        trans.anchoredPosition = Camera.main.WorldToScreenPoint(new Vector2(transform.position.x, transform.position.y));
+        trans.pivot = new Vector2(-9, -5);
 
         text = gameObject.AddComponent<Text>();
         text.text = "hello";
@@ -25,5 +28,11 @@ public class UnitDisplayer : MonoBehaviour
         //int units = GameManagerScript.Towers[towerIndex].UnitsInside;
 
         // Display units
+    }
+
+    private float ConvertUnit2Px(float unit) {
+        float ortho = cam.orthographicSize;
+        float pixelH = cam.pixelHeight;
+        return (unit * pixelH)/(ortho * 2);
     }
 }

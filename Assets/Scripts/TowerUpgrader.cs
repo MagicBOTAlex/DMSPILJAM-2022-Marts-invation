a61685e93d15bdd9574773d90eff01abe9b54c0d
benjamus <carl.benjamin.dreyer@gmail.com>
2022-03-05 01:54:12 +0100

    fin

--- Assets/Scripts/TowerUpgrader.cs
+++ Assets/Scripts/TowerUpgrader.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class TowerUpgrader : MonoBehaviour
 {
-    
+    private const float killDelay = 3f;
+    private void Start() {
+        StartCoroutine(KillMenu(killDelay));
+    }
     private void OnMouseDown() {
+        Upgrade();
 
+    }
+
+    private void Upgrade() {
+        // Get the current tower index
+        
+    }
+    private IEnumerator KillMenu(float dl) {
+        yield return new WaitForSecondsRealtime(dl);
+        Destroy(gameObject);
     }
 }

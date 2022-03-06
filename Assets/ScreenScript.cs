8dde4ce744d7809d0ee91a0211d7882c844ceb2d
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-03-06 17:03:49 +0100

    321

--- Assets/ScreenScript.cs
+++ Assets/ScreenScript.cs
@@ -6,7 +6,7 @@ public class ScreenScript : MonoBehaviour
 {
     private void Start()
     {
-        Screen.SetResolution((int)(1920 / 1.5), (int)(1080/1.5), false);
+        //Screen.SetResolution((int)(1920 / 1.5), (int)(1080/1.5), false);
         //Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
     }
 

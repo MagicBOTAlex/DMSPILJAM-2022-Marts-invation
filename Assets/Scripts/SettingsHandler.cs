f3c26eed2f62322a210cbbc620bf5a6aec5bca68
benjamus <carl.benjamin.dreyer@gmail.com>
2022-03-06 16:26:36 +0100

    s

--- Assets/Scripts/SettingsHandler.cs
+++ Assets/Scripts/SettingsHandler.cs
@@ -12,13 +12,20 @@ public class SettingsHandler : MonoBehaviour
 
     private void Awake() {
         soundSlider = transform.GetChild(0).GetComponent<Slider>();
-        soundSlider.value = PlayerPrefs.GetFloat("Volume");
+        if (PlayerPrefs.GetInt("isVolumeSet") == 0) {
+            Debug.Log("NEW VALUE SETTING 1");
+            soundSlider.value = 1f;
+        }
+        else {
+            soundSlider.value = PlayerPrefs.GetFloat("Volume");
+        }
     }
 
 
     private void FixedUpdate() {
         // Set volume
         PlayerPrefs.SetFloat("Volume", soundSlider.value);
+        PlayerPrefs.SetInt("isVolumeSet", 1);
         AudioListener.volume = PlayerPrefs.GetFloat("Volume");
     }
 }
